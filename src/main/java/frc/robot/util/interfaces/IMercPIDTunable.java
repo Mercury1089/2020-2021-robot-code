a994d71c314888169338abcf3c830513877c2000
DrewTheRat <HappyJ0y!>
2020-01-20 15:36:42 -0500

    PID Tuner added to ShuffleDash

--- src/main/java/frc/robot/util/interfaces/IMercPIDTunable.java
+++ src/main/java/frc/robot/util/interfaces/IMercPIDTunable.java
@@ -3,7 +3,6 @@ package frc.robot.util.interfaces;
 import frc.robot.util.PIDGain;
 
 public interface IMercPIDTunable {
-    public String getPIDName();
-    public PIDGain getPIDGain();
-    public void setPIDGain(PIDGain gain);
+    public PIDGain getPIDGain(String slot);
+    public void setPIDGain(String slot, PIDGain gain);
 }
