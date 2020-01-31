333551ec41f596ec36213da8b07d5427c194fbf3
Adog64ツ <33038791+Adog64@users.noreply.github.com>
2020-01-31 16:42:46 -0500

    Rewrite of rotation control only a few errors left

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -124,7 +124,7 @@ public class RobotContainer {
         right2.whileHeld(new RunFeeder(feeder));
         right3.whileHeld(new DriveElevator(elevator));
         right4.whenPressed(new DriveWithJoysticks(DriveType.ARCADE, driveTrain));
-        right5.whileHeld(new RotationControl(spinner));
+        right5.whenPressed(new RotationControl(spinner));
         right6.whenPressed(new MoveHeading(0, 90, driveTrain));
         right7.whenPressed(new DegreeRotate(45, driveTrain));
         right8.whenPressed(new DegreeRotate(90, driveTrain));
--- src/main/java/frc/robot/commands/spinner/RotationControl.java
+++ src/main/java/frc/robot/commands/spinner/RotationControl.java
@@ -9,12 +9,13 @@ package frc.robot.commands.spinner;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.sensors.REVColor.ControlPanelColor;
 import frc.robot.subsystems.Spinner;
 
 public class RotationControl extends CommandBase {
   
   private Spinner spinner;
-  private Color previousColor, currentColor;
+  private ControlPanelColor previousColor, currentColor;
 
   private int colorsCrossed;
 
@@ -28,21 +29,16 @@ public class RotationControl extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    previousColor = spinner.getDetectedColor();
-    currentColor = previousColor;
+    spinner.setSpeed(spinner.getRunSpeed());
+    colorsCrossed = 0;
+    currentColor = spinner.getColorSensor().get();
+    previousColor = currentColor;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    currentColor = spinner.getDetectedColor();
-
-    if(!currentColor.equals(previousColor)) {
-      colorsCrossed++;
-      previousColor = currentColor;
-    }
-
-    spinner.setSpeed(spinner.getRunSpeed());
+    updateColor();
   }
 
   // Called once the command ends or is interrupted.
@@ -55,5 +51,37 @@ public class RotationControl extends CommandBase {
   @Override
   public boolean isFinished() {
     return colorsCrossed > MINIMUM_COLORS_CROSSED;
+  }
+
+  public void updateColor() {
+    ControlPanelColor sensorColor = spinner.getColorSensor().get();
+    if (previousColor == ControlPanelColor.YELLOW) {
+      if (sensorColor == ControlPanelColor.RED) {
+        previousColor = currentColor;
+        currentColor = ControlPanelColor.RED;
+        colorsCrossed++;
+      }
+    } 
+    else if (previousColor == ControlPanelColor.RED) {
+      if (sensorColor == ControlPanelColor.GREEN) {
+        previousColor = currentColor;
+        currentColor = ControlPanelColor.GREEN;
+        colorsCrossed++;
+      }
+    }
+    else if(previousColor == ControlPanelColor.GREEN) {
+      if (sensorColor == ControlPanelColor.BLUE) {
+        previousColor = currentColor;
+        currentColor = ControlPanelColor.BLUE;
+        colorsCrossed++;
+      }
+    }
+    else if(previousColor == ControlPanelColor.BLUE) {
+      if(sensorColor == ControlPanelColor.YELLOW) {
+        previousColor = currentColor;
+        currentColor = ControlPanelColor.YELLOW;
+        colorsCrossed++;
+      }
+    }
   }
 }
