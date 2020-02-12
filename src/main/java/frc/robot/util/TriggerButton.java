0e8e70eb2aef46f0505b8ffc0cca291c8d719912
Adog64ツ <33038791+Adog64@users.noreply.github.com>
2020-02-12 12:22:27 -0500

    TriggerButton simplified

--- src/main/java/frc/robot/util/TriggerButton.java
+++ src/main/java/frc/robot/util/TriggerButton.java
@@ -1,154 +1,56 @@
 package frc.robot.util;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.button.Button;
 
-public class TriggerButton {
+public class TriggerButton extends Button{
 
-  private Joystick gamepad;
-  private int trigger;
-  private boolean held = false;
-  private final double PRESS_THRESHOLD;
+  private static final double DEFAULT_THRESHOLD = 0.5;
 
-  public TriggerButton(Joystick gamepad, int trigger) {
-    this(gamepad, trigger, 0.1);
-  }
-
-  public TriggerButton(Joystick gamepad, int trigger, double press_threshold) {
-    this.gamepad = gamepad;
-    this.trigger = trigger;
-    PRESS_THRESHOLD = press_threshold;
-  }
-  
-  /**
-   * @return whether or not the trigger is pressed past the set threshold
-   */
-  public boolean isPressed(){
-    if(gamepad.getRawAxis(trigger) > PRESS_THRESHOLD)
-      return true;
-    return false;
-  }
+  private final GenericHID m_joystick;
+  private final int m_axisNumber;
+  private final boolean m_polarity;
+  private final double m_threshold;
 
   /**
-   * Starts the command when the trigger is pressed, won't run again until the button is released and pressed again.
-   * @param command the command to run
+   * Create a gamepad axis for triggering commands as if it were a button.
+   *
+   * @param joystick The GenericHID object that has the axis (e.g. Joystick, KinectStick, etc)
+   * 
+   * @param axisNumber The axis number (see {@link GenericHID#getRawAxis(int) }
+   * 
+   * @param polarity The polarity of the axis (to specify which direction is considered the positive direction)
+   * 
+   * @param threshold The threshold above which the axis shall trigger a command
    */
-  public void whenPressed(Command command) {
-    whenPressed(command, true);
+  public TriggerButton(GenericHID joystick, int axisNumber, boolean polarity, double threshold) {
+    m_joystick = joystick;
+    m_axisNumber = axisNumber;
+    m_polarity = polarity;
+    m_threshold = threshold;
   }
 
-  /**
-   * Starts the command when the trigger is pressed, won't run again until the button is released and pressed again.
-   * @param command the command to run
-   * @param interruptible whether or not the command is interruptible
-   */
-  public void whenPressed(Command command, boolean interruptible) {
-    CommandScheduler.getInstance().addButton(new Runnable() {
-  
-      @Override
-      public void run() {
-        boolean pressed = isPressed();
+  public TriggerButton(GenericHID joystick, int axisNumber, boolean polarity) {
+    this(joystick, axisNumber, polarity, DEFAULT_THRESHOLD);
+  }
 
-        if(pressed && !command.isScheduled()) {
-          command.schedule(interruptible);
-        }
-      }
-    });
+  public TriggerButton(GenericHID joystick, int axisNumber) {
+    this(joystick, axisNumber, true);
   }
 
   /**
-   * Starts the command when the trigger is pressed, ends it when it is released.
-   * @param command the command to run
-   * @param interruptible whether or not the command is interruptible
+   * Gets the value of the gamepad axis.
+   *
+   * @return The value of the gamepad axis
    */
-  public void whenHeld(Command command, boolean interruptible) {
-    CommandScheduler.getInstance().addButton(new Runnable() {
-  
-      @Override
-      public void run() {
-        boolean pressed = isPressed();
+  public boolean get() {
+    double rawAxis = m_joystick.getRawAxis(m_axisNumber);
 
-        if (pressed && !held) {
-          command.schedule(interruptible);
-          held = true;
-        } else if (!pressed) {
-          command.cancel();
-          held = false;
-        }
-      }
-    });
-  }
-
-  /**
-   * Starts the command when the trigger is pressed, ends it when it is released.
-   * @param command the command to run
-   */
-  public void whenHeld(Command command) {
-    whenHeld(command, true);
-  }
-
-  /**
-   * Runs the command repeatedly while the trigger is held.
-   * @param command the command to run
-   * @param interruptible whether or not the command is interruptible
-   */
-  public void whileHeld(Command command, boolean interruptible) {
-    CommandScheduler.getInstance().addButton(new Runnable() {
-  
-      @Override
-      public void run() {
-        boolean pressed = isPressed();
-
-        if (pressed) {
-          command.schedule(interruptible);
-        } 
-        else if (!pressed) {
-          command.cancel();
-        }
-      }
-    });
-  }
-
-  /**
-   * Runs the command repeatedly while the trigger is held.
-   * @param command the command to run
-   */
-  public void whileHeld(Command command) {
-    whileHeld(command, true);
-  }
-    
-  /**
-   * Starts the command when the trigger is pressed pressed, stops the command when it is pressed again.
-   * @param command the command to run
-   */
-  public void toggleWhenPressed(Command command) {
-    toggleWhenPressed(command, true);
-  }
-
-  /**
-   * Starts the command when the trigger is pressed pressed, stops the command when it is pressed again.
-   * @param command the command to run
-   * @param interruptible whether or not the command is interruptible
-   */
-  public void toggleWhenPressed(Command command, boolean interruptible) {
-    CommandScheduler.getInstance().addButton(new Runnable() {
-  
-      @Override
-      public void run() {
-        boolean pressed = isPressed();
-
-        if (pressed && !held) {
-          System.out.println("Pressed to toggle");
-          if(command.isScheduled())
-            command.cancel();
-          else
-            command.schedule(interruptible);
-          held = true;
-        }
-        else if(!pressed)
-          held = false;
-      }
-    });
+    if (m_polarity) {
+			return (Math.max(rawAxis,0) > m_threshold);				
+    } 
+    else  {
+			return (-Math.min(rawAxis,0) > m_threshold);
+		}
   }
 }
