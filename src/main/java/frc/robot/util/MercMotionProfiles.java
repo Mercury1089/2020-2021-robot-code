6334010654e7f7e249408b4b6b0d0764c6ad486f
Mercury1089 <1089software@gmail.com>
2020-02-12 04:13:00 -0500

    Motion Profile objects!

---
+++ src/main/java/frc/robot/util/MercMotionProfiles.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ctre.phoenix.motion.TrajectoryPoint;
+
+/**
+ * Add your docs here.
+ */
+public class MercMotionProfiles {
+    private final String name;
+    private final String pathDirectory;
+    List<TrajectoryPoint> trajectoryPoints;
+
+    public MercMotionProfiles(String name) {
+        this.name = name;
+        pathDirectory = MercPathLoader.getBasePathLocation() + name + ".wpilib.json"; 
+        trajectoryPoints = MercPathLoader.loadPath(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPathDirectory() {
+        return pathDirectory;
+    }
+
+    public List<TrajectoryPoint> getTrajectoryPoints() {
+        return trajectoryPoints;
+    }
+
+    public List<TrajectoryPoint> getPathForward() {
+        return trajectoryPoints;
+    }
+
+    public List<TrajectoryPoint> getPathBackwards() {
+        List<TrajectoryPoint> points = new ArrayList<TrajectoryPoint>();
+        for (int i = trajectoryPoints.size() - 1; i >= 0; i--) {
+            TrajectoryPoint p = trajectoryPoints.get(i);
+            p.velocity *= -1;
+           // p.headingDeg = 90 - p.headingDeg;
+           points.add(p);
+        }
+        return points;
+    }
+}
