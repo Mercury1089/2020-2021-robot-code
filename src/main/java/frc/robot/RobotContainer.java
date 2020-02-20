c6bfea87317ced9c8ec89432745de2213098918d
Mercury1089 <1089software@gmail.com>
2020-02-20 06:05:05 -0500

    removed unused logic

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -145,12 +145,12 @@ public class RobotContainer {
         right8.whenPressed(new DriveDistance(120.0, driveTrain));
         right9.whenPressed(new RotateToTarget(driveTrain, limelightCamera));
         try {
-            right10.whenPressed(new MoveOnTrajectory(straight, driveTrain/*, false*/));            
+            right10.whenPressed(new MoveOnTrajectory(straight, driveTrain));            
         } catch(FileNotFoundException e) {
             System.out.println(e);
         }
         try {
-            right11.whenPressed(new MoveOnTrajectory(straight, driveTrain/*, true*/));            
+            right11.whenPressed(new MoveOnTrajectory(curvy, driveTrain));            
         } catch(FileNotFoundException e) {
             System.out.println(e);
         }
@@ -245,21 +245,21 @@ public class RobotContainer {
     }
 
     public void initializeMotionProfiles() {
-        circle = new MercMotionProfile("Circle");
-        curveBack = new MercMotionProfile("CurveBack");
-        curvy = new MercMotionProfile("Curvy");
-        digonal = new MercMotionProfile("Digonal");
-        hardRight = new MercMotionProfile("HardRight");
-        straight = new MercMotionProfile("Straight");
-        targetZoneToRendezvousBalls = new MercMotionProfile("TargetZoneToRendezvousBalls");
-        targetZoneToTrench = new MercMotionProfile("TargetZoneToTrench");
+        circle = new MercMotionProfile("Circle", false);
+        curveBack = new MercMotionProfile("CurveBack", false);
+        curvy = new MercMotionProfile("Curvy", true);
+        digonal = new MercMotionProfile("Digonal", false);
+        hardRight = new MercMotionProfile("HardRight", false);
+        straight = new MercMotionProfile("Straight", true);
+        targetZoneToRendezvousBalls = new MercMotionProfile("TargetZoneToRendezvousBalls", false);
+        targetZoneToTrench = new MercMotionProfile("TargetZoneToTrench", false);
     }
 
     //Eventually this will link to our auton app on the shuffledash
     public void initializeAutonCommand(){
         autonCommand.addRequirements(this.driveTrain);
         try {
-            autonCommand.addCommands(new MoveOnTrajectory(straight, this.driveTrain/*, false*/));            
+            autonCommand.addCommands(new MoveOnTrajectory(straight, this.driveTrain));            
         } catch(FileNotFoundException e) {
             System.out.println(e);
         }
